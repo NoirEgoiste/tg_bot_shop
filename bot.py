a6af9fe4acacab382b2332d72313d909a1dd0d15
Noir_Egoiste <dmitreykyrchin@gmail.com>
2024-05-22 22:47:02 +0300

    include admin router

--- bot.py
+++ bot.py
@@ -3,11 +3,17 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from config import TOKEN
+from app.database.models import async_main
+from app.handlers import router
+from app.admin import admin as admin_router
 
 
 async def main():
+    await async_main()
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
+    dp.include_routers(admin_router, router)
+
     
     await dp.start_polling(bot)    
 
@@ -17,4 +23,4 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        logging.info('Ctrl+C pressed. Stopping.')
+        logging.info('Ctrl+C pressed. Exiting.')
