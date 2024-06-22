9e4b4bc83b4950e9d442f195540d10079664dcf3
Noir_Egoiste <dmitreykyrchin@gmail.com>
2024-06-22 09:02:01 +0300

    add cart requests

--- app/database/requests.py
+++ app/database/requests.py
@@ -12,17 +12,31 @@ async def set_user(tg_id):
             session.add(User(tg_id=tg_id))
             await session.commit()
 
-async def get_users():
-    async with async_session() as session:
-        users = await session.scalars(select(User))
-        return users
-
 
 async def set_item(data):
     async with async_session() as session:
         session.add(Item(**data))
         await session.commit()
 
+async def set_cart(tg_id, item_id):
+    async with async_session() as session:
+        user = await session.scalar(select(User).where(User.tg_id == tg_id))
+        session.add(Cart(user=user.id, item=item_id))
+        await session.commit()
+
+
+async def get_users():
+    async with async_session() as session:
+        users = await session.scalars(select(User))
+        return users
+
+
+
+async def get_cart(tg_id):
+    async with async_session() as session:
+        user = await session.scalar(select(User).where(User.tg_id == tg_id))
+        cart = await session.scalars(select(Cart).where(Cart.user == user.id))
+    return cart
 
 async def get_categories():
     async with async_session() as session:
@@ -30,7 +44,7 @@ async def get_categories():
         return categories
 
 
-async def get_items_by_category(category_id: int):
+async def get_items_by_category(category_id):
     async with async_session() as session:
         items = await session.scalars(
             select(Item).where(Item.category == category_id)
@@ -38,9 +52,17 @@ async def get_items_by_category(category_id: int):
         return items
 
 
-async def get_items_by_id(item_id: int):
+async def get_items_by_id(item_id):
     async with async_session() as session:
         item = await session.scalar(
             select(Item).where(Item.id == item_id)
         )
         return item
+
+async def delete_cart(tg_id, item_id):
+    async with async_session() as session:
+        user = await session.scalar(select(User).where(User.tg_id == tg_id))
+        await session.execute(delete(Cart).where(Cart.user == user.id,
+                                                 Cart.item == item_id))
+        await session.commit()
+
