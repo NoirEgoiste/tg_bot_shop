2037bf916528b124b145b281e577e548a773682e
Noir_Egoiste <dimon0.0@yandex.ru>
2024-06-24 08:53:46 +0300

    Add for admin panel add_category (not working now)

--- app/admin.py
+++ app/admin.py
@@ -1,11 +1,11 @@
 from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
-from aiogram.filters import CommandStart, Command, Filter
+from aiogram.filters import Command, Filter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
 import app.keyboards as keyboards
-from app.database.requests import get_users, set_item
+from app.database.requests import get_users, set_item, set_new_category
 
 admin = Router()
 
@@ -22,15 +22,21 @@ class AddItem(StatesGroup):
     price = State()
 
 
+class AddCategory(StatesGroup):
+    name = State()
+
+
 class AdminProtect(Filter):
     async def __call__(self, message: Message):
-        return message.from_user.id in [239721784] # Tg admin id's
+        return message.from_user.id in [239721784]  # Tg admin id's
+
 
 @admin.message(AdminProtect(), Command("admin"))
 async def admin_panel(message: Message):
     await message.answer(f"Commands: \n"
                          f"/newsletter\n"
-                         f"/add_item\n")
+                         f"/add_item\n"
+                         f"/add_category\n")
 
 
 @admin.message(AdminProtect(), Command("newsletter"))
@@ -45,10 +51,20 @@ async def newsletter_message(message: Message, state: FSMContext):
     for user in await get_users():
         try:
             await message.send_copy(chat_id=user.tg_id)
-        except:
-            pass
+        except Exception as e:
+            print(f"Not implemented {e}")
     await message.answer("Mailing Ended")
     await state.clear()
+
+
+# Todo отсутствует возможность добавить новые категории, функция есть но не работает должным образом.
+@admin.message(AdminProtect(), Command("add_category"), AddCategory.name)
+async def add_category(message: Message, state: FSMContext):
+    # await state.set_state(AddCategory.name)
+    await message.answer("Enter category to add")
+    # await state.update_data(category=message.text)
+    await set_new_category(message.text)
+    await message.answer("Added successfully")
 
 
 @admin.message(AdminProtect(), Command("add_item"))
@@ -73,7 +89,6 @@ async def add_item_category(callback: CallbackQuery, state: FSMContext):
     await callback.message.answer("Enter Item description")
 
 
-
 @admin.message(AdminProtect(), AddItem.description)
 async def add_item_description(message: Message, state: FSMContext):
     await state.update_data(description=message.text)
@@ -86,7 +101,6 @@ async def add_item_photo(message: Message, state: FSMContext):
     await state.update_data(photo=message.photo[-1].file_id)
     await state.set_state(AddItem.price)
     await message.answer("Enter Item price")
-
 
 
 @admin.message(AdminProtect(), AddItem.price)
