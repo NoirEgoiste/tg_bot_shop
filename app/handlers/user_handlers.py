c454b3d754916b385815ae63c71714fc074250f0
Noir_Egoiste <dimon0.0@yandex.ru>
2024-07-21 03:05:54 +0300

    create keyboards, revork handlers

--- app/handlers/user_handlers.py
+++ app/handlers/user_handlers.py
@@ -1,29 +1,36 @@
 from aiogram import Router, F
 from aiogram.enums import ChatType
-from aiogram.filters import Command, or_f
+from aiogram.filters import CommandStart, Command, or_f
 from aiogram.types import Message, CallbackQuery
 
 from app.filters.chat_type_filter import ChatTypeFilter
+from app.keyboards.reply_keyboards import start_kb, del_kb, start_kb2, start_kb3, start_kb4
+
 
 user = Router()
 user.message.filter(ChatTypeFilter([ChatType.PRIVATE]))
 
 
+@user.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer(text="Привет я виртуальный помощник!", reply_markup=start_kb4)
+
+
 @user.message(or_f(Command("menu"), F.text.lower() == "меню"))
 async def main_menu(message: Message):
-    await message.answer(text="Hello")
+    await message.answer(text="Вот меню:", reply_markup=del_kb)
 
 
-@user.message(or_f(Command("about"), F.text.lower() == "о нас"))
+@user.message(or_f(Command("about"), F.text.lower() == "о магазине"))
 async def about(message: Message):
-    await message.answer(text="About")
+    await message.answer(text="О нас:")
 
 
 @user.message(or_f(Command("payment"), F.text.lower() == "оплата"))
 async def payment(message: Message):
-    await message.answer(text="Payment")
+    await message.answer(text="Оплата:")
 
 
 @user.message(or_f(Command("shipment"), F.text.lower() == "доставка"))
 async def shipment(message: Message):
-    await message.answer(text="Magic filter")
+    await message.answer(text="Доставка:")
---
+++ app/keyboards/reply_keyboards.py
@@ -0,0 +1,35 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, KeyboardButtonPollType
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+start_kb = ReplyKeyboardMarkup(keyboard=[
+        [KeyboardButton(text="Меню"),
+        KeyboardButton(text="О магазине"),],
+        [KeyboardButton(text="Доставка"),
+        KeyboardButton(text="Оплата"),]    
+    ], 
+    resize_keyboard=True,
+    input_field_placeholder="Выберите вариант",
+)
+
+del_kb = ReplyKeyboardRemove()
+
+
+start_kb2 = ReplyKeyboardBuilder()
+start_kb2.add(
+    KeyboardButton(text="Меню"),
+    KeyboardButton(text="О магазине"),
+    KeyboardButton(text="Доставка"),
+    KeyboardButton(text="Оплата"),
+)
+start_kb2.adjust(2, 2)
+
+start_kb3 = ReplyKeyboardBuilder()
+start_kb3.attach(start_kb2)
+start_kb3.row(KeyboardButton(text="Hello"))
+
+
+start_kb4 = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="Создать опрос", request_poll=KeyboardButtonPollType())],
+    [KeyboardButton(text="☎️ Отправить номер телефона", request_contact=True),
+    KeyboardButton(text="🗺️ Отправить местоположение", request_location=True),]
+], resize_keyboard=True,)
